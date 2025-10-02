6cc4a1aae6dbf65c992632dadc22c0ce40b37261
User <user@example.com>
2025-10-01 22:46:09 -0400

    Remove Sign up button linking to plus.excalidraw.com from welcome screen

--- excalidraw-app/components/AppWelcomeScreen.tsx
+++ excalidraw-app/components/AppWelcomeScreen.tsx
@@ -1,4 +1,3 @@
-import { loginIcon } from "@excalidraw/excalidraw/components/icons";
 import { POINTER_EVENTS } from "@excalidraw/common";
 import { useI18n } from "@excalidraw/excalidraw/i18n";
 import { WelcomeScreen } from "@excalidraw/excalidraw/index";
@@ -31,15 +30,6 @@ export const AppWelcomeScreen: React.FC<{
               onSelect={() => props.onCollabDialogOpen()}
             />
           )}
-          <WelcomeScreen.Center.MenuItemLink
-            href={`${
-              import.meta.env.VITE_APP_PLUS_LP
-            }/plus?utm_source=excalidraw&utm_medium=app&utm_content=welcomeScreenGuest`}
-            shortcut={null}
-            icon={loginIcon}
-          >
-            Sign up
-          </WelcomeScreen.Center.MenuItemLink>
         </WelcomeScreen.Center.Menu>
       </WelcomeScreen.Center>
     </WelcomeScreen>
